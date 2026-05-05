ab37a8922d59e55a445da10155e0a002c8becf12
Anderpott33 <anderpott33@gmail.com>
2026-05-05 01:00:52 -0300

    arreglo de cors

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ const app: Application = express();
 // Puedes dejarlo vacío app.use(cors()) para permitir todo, 
 // o ser específico (más seguro):
 app.use(cors({
-  origin: 'http://localhost:5174', // Tu URL de Frontend (Vite)
+  origin: '*', // Tu URL de Frontend (Vite)
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
